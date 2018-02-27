2e65e4daca73d5f1da92db7a4d92cb3006178eac
Alex <awu1337@gmail.com>
2018-02-27 17:14:23 -0600

    Added POI GET methods

--- api/views/pois.py
+++ api/views/pois.py
@@ -1,24 +1,44 @@
 from api import app
 from flask import Blueprint, request, jsonify
-from api.models import POI
+from api.models import POI, Media, Link, Story, StoryPOI
 import json
 from api.utils import create_response, InvalidUsage
 
 mod = Blueprint('POIS', __name__)
 
+def poi_links_media_stories(poi_dict):
+    """
+    Gets additional links, media, and stories for a POI
+    """
+    poi_links = Link.query.filter(Link.poi_id == poi_dict['_id'])
+    poi_media = Media.query.filter(Media.poi_id == poi_dict['_id'])
+    poi_stories = Story.query.join(StoryPOI, Story.id == StoryPOI.story_id).filter(StoryPOI.poi_id == poi_dict['_id'])
+    poi_dict['links'] = [j.to_dict() for j in poi_links]
+    poi_dict['media'] = [j.to_dict() for j in poi_media]
+    poi_dict['stories'] = [j.to_dict() for j in poi_stories]
+    return poi_dict
+
 @app.route('/pois', methods=['GET'])
 def pois_get_by_map_year_or_story():
-    map_year = request.args.get('map_year')
-    story_id = request.args.get('story_id')
+    map_year, story_id = request.args.get('map_year'), request.args.get('story_id')
     if map_year is not None and story_id is None:
-        #find pois by map year
+        pois = POI.query.filter(POI.map_year == int(map_year))
+        if pois.count() == 0:
+            return create_response(status=404, message='No POIs found')
     elif story_id is not None and map_year is None:
-        #find pois by story_id
-    return create_response(data)
+        pois = POI.query.join(StoryPOI, POI.id == StoryPOI.poi_id).filter(StoryPOI.story_id == int(story_id))
+        if pois.count() == 0:
+            return create_response(status=404, message='No POIs found')
+    pois_list = [poi_links_media_stories(i.to_dict()) for i in pois]
+    return create_response({'pois': pois_list})
 
 @app.route('/pois/<poi_id>', methods=['GET'])
 def pois_get_by_id(poi_id):
-    return create_response(data)
+    poi_by_id = POI.query.get(int(poi_id))
+    if poi_by_id is None:
+        return create_response(status=404, message='No POI found')
+    poi_result = poi_links_media_stories(poi_by_id.to_dict())
+    return create_response({'poi': poi_result})
 
 @app.route('/pois', methods=['POST'])
 def pois_post():
