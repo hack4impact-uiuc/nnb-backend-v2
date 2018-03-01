125e62a725b6360c7fd44e4b63143b63b3a948dc
wangworks <policetoed@gmail.com>
2018-02-28 23:10:11 -0600

    get probably works

--- api/views/stories.py
+++ api/views/stories.py
@@ -1,5 +1,5 @@
 from api import app, db
-from api.models import POI, Media, Link, Story, StoryPOI
+from api.models import Story, StoryPOI
 from api.utils import create_response, InvalidUsage
 from sqlalchemy.sql.expression import func
 from flask import Blueprint, request, jsonify
@@ -11,12 +11,13 @@ mod = Blueprint('stories', __name__)
 
 GET_STORIES = "/stories"
 
-# @app.route(GET_STORIES, methods=['GET'])
-# def get_stories():
-#     if 'POI' in request.args:
-#         stories = STORY_POI.query.filter(STORY_POI.poi_id == request.args.get('POI'))
-#         if stories.count() == 0:
-#             return create_response(status=404, message='No stories found for specified POI')
-#         else return create_response(stories)
-#     else:
-#         return create_response(STORY.query)
+@app.route(GET_STORIES, methods=['GET'])
+def get_stories():
+    if 'POI' in request.args:
+        stories = StoryPOI.query.filter(StoryPOI.poi_id == request.args.get('POI'))
+        if stories.count() == 0:
+            return create_response(status=404, message='No stories found for specified POI')
+        else:
+            return create_response({'stories':stories})
+    else:
+        return create_response({'stories':Story.query.all()})
