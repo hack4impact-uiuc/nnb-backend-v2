068e7043773c6ef1721ef4adad006cd8a9f05a61
jeffylin <jlin79@illinois.edu>
2018-02-28 23:28:21 -0600

    added post stories

--- api/views/stories.py
+++ api/views/stories.py
@@ -9,7 +9,7 @@ from dateutil.parser import parse
 
 mod = Blueprint('stories', __name__)
 
-GET_STORIES = "/stories"
+STORIES = "/stories"
 
 # @app.route(GET_STORIES, methods=['GET'])
 # def get_stories():
@@ -20,3 +20,19 @@ GET_STORIES = "/stories"
 #         else return create_response(stories)
 #     else:
 #         return create_response(STORY.query)
+
+def post_stories():
+	new_story = Story(story_name = request.args.get('story_name'))
+	db.session.add(new_story)
+	if(not request.args.get('poi_ids') == False):
+		ids = request.args.get('poi_ids')
+		for id in ids:
+			new_story_poi = StoryPOI(story_id = new_story.id, poi_id = id)
+			db.session.add(new_story_poi)
+	db.session.commit()
+	return create_reponse({'story': new_story})
+
+
+
+
+
