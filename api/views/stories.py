06974050f067aa6da134f467749ed287aa738a5b
jeffylin <jlin79@illinois.edu>
2018-03-07 20:47:32 -0600

    made changes from comments

--- api/views/stories.py
+++ api/views/stories.py
@@ -1,66 +1,58 @@
 from api import app, db
 from api.models import Story, StoryPOI
-from api.utils import create_response, InvalidUsage
-from sqlalchemy.sql.expression import func
-from flask import Blueprint, request, jsonify
-import json
-from datetime import date
-from dateutil.parser import parse
+from api.utils import create_response
+from flask import Blueprint, request
 
 mod = Blueprint('stories', __name__)
 
-STORIES = "/stories"
-STORIES_PARAM = "/stories/<story_id>"
+STORIES_URL = "/stories"
+STORIES_ID_URL = "/stories/<int:story_id>"
 
-@app.route(STORIES, methods=['GET'])
+@app.route(STORIES_URL, methods=['GET'])
 def get_stories():
     if 'POI' in request.args:
         stories = StoryPOI.query.filter(StoryPOI.poi_id == request.args.get('POI'))
         if stories.count() == 0:
             return create_response(status=404, message='No stories found for specified POI')
         else:
-            return create_response({'stories':stories})
+            return create_response({'stories': [s.to_dict() for s in stories]})
     else:
         stories = Story.query.all()
         return create_response({'stories': [s.to_dict() for s in stories]})
 
-@app.route(STORIES, methods=['POST'])
+@app.route(STORIES_URL, methods=['POST'])
 def post_stories():
     data = request.get_json()
-    new_story = Story(story_name = data['story_name'])
+    new_story = row_constructor(Story, data)
     db.session.add(new_story)
     db.session.flush()
 
-    data['poi_ids'] = data['poi_ids'] if 'poi_ids' in data else []
-    if(len(data['poi_ids']) != 0):
-        ids = data['poi_ids']
-        for i in ids:
-            new_story_poi = StoryPOI(story_id = new_story.id, poi_id = i)
-            db.session.add(new_story_poi)
+    if 'poi_ids' in data:
+    	new_story_pois = [row_constructor(StoryPOI, story_id=new_story.id, poi_id=poi_id) for poi_id in data['poi_ids']]
+    	db.session.add_all(new_story_pois)
     db.session.commit()
     story_dict = new_story.to_dict()
     story_dict['id'] = new_story.id
     return create_response(data = {'story': story_dict}, message = 'Story created')
 
-@app.route(STORIES_PARAM, methods=['PUT'])
+@app.route(STORIES_ID_URL, methods=['PUT'])
 def put_stories(story_id):
     data = request.get_json()
-    new_story_name = data['story_name'] if 'story_name' in data else ''
-    new_poi_ids = data['poi_ids'] if 'poi_ids' in data else []
     story = Story.query.get(story_id)
-    if(len(new_story_name) != 0):
-    	story.story_name = new_story_name
-    StoryPOI.query.filter(StoryPOI.story_id == story_id).delete()
-    if(len(new_poi_ids) != 0):
-    	for i in new_poi_ids:
-    		new_story_poi = StoryPOI(story_id = story_id, poi_id = i)
-    		db.session.add(new_story_poi)
-    db.session.flush()
+    if 'story_name' in data:
+    	story.story_name = data['story_name']
+    if 'poi_ids' in data:
+    	if(len(data['poi_ids']) > 0):
+    		for i in new_poi_ids:
+    			new_story_pois = [row_constructor(StoryPOI, story_id = story_id, poi_id = poi_id) for poi_id in data['poi_ids']]
+    			db.session.add_all(new_story_pois)
+   		else:
+   			StoryPOI.query.filter(StoryPOI.story_id == story_id).delete()
     db.session.commit()
     return create_response(data = {'story': story.to_dict()}, message = 'Story updated')
 
 
-@app.route(STORIES_PARAM, methods = ['DELETE'])
+@app.route(STORIES_ID_URL, methods = ['DELETE'])
 def delete_stories(story_id):
     to_delete = Story.query.get(story_id)
     db.session.delete(to_delete)
