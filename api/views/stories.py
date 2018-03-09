cfb65005181438a64ea26bae8f819b9ad7f52343
unknown <jlin79@illinois.edu>
2018-03-09 00:29:06 -0600

    fixed comments

--- api/views/stories.py
+++ api/views/stories.py
@@ -10,8 +10,9 @@ STORIES_ID_URL = "/stories/<int:story_id>"
 
 @app.route(STORIES_URL, methods=['GET'])
 def get_stories():
-    if 'POI' in request.args:
-        stories = StoryPOI.query.filter(StoryPOI.poi_id == request.args.get('POI'))
+    data = request.get_json()
+    if 'poi_id' in data:
+        stories = StoryPOI.query.filter(StoryPOI.poi_id == data['poi_id'])
         if stories.count() == 0:
             return create_response(status=404, message='No stories found for specified POI')
         else:
@@ -23,7 +24,10 @@ def get_stories():
 @app.route(STORIES_URL, methods=['POST'])
 def post_stories():
     data = request.get_json()
-    new_story = row_constructor(Story, data)
+    if 'story_name' in data:
+        new_story = row_constructor(Story, data)
+    else:
+        return create_response(status=422, message='No story_name parameter was provided')
     db.session.add(new_story)
     db.session.flush()
 
@@ -45,13 +49,15 @@ def put_stories(story_id):
         if(len(data['poi_ids']) > 0):
             new_story_pois = [row_constructor(StoryPOI, story_id = story_id, poi_id = poi_id) for poi_id in data['poi_ids']]
             db.session.add_all(new_story_pois)
-    db.session.commit()
+            db.session.commit()
     story = Story.query.get(story_id)
     return create_response(data = {'story': story.to_dict()}, message = 'Story updated')
 
 @app.route(STORIES_ID_URL, methods = ['DELETE'])
 def delete_stories(story_id):
     to_delete = Story.query.get(story_id)
+    if to_delete.count() == 0:
+        return create_response(status=404, message='Provided story_id does not exist')
     db.session.delete(to_delete)
     StoryPOI.query.filter(StoryPOI.story_id == story_id).delete()
     db.session.commit()
--- tests/test_stories.py
+++ tests/test_stories.py
@@ -9,57 +9,57 @@ from flask import jsonify
 import json
 
 poi1 = {
-		  '_id' = 1
-          'name': 'Himalayan Chimney',
-          'date': 'Date(2018, 2, 27)',
-          'description': 'Yum',
-          'map_year': 2018,
-          'x_coord': 12,
-          'y_coord': 43
-      }
+    '_id' = 1
+    'name': 'Himalayan Chimney',
+    'date': 'Date(2018, 2, 27)',
+    'description': 'Yum',
+    'map_year': 2018,
+    'x_coord': 12,
+    'y_coord': 43
+}
 
 poi2 = {
-		  '_id' = 2
-          'name': 'Mount Everest',
-          'date': 'Date(2018, 5, 15)',
-          'description': 'Snow',
-          'map_year': 2018,
-          'x_coord': 15,
-          'y_coord': 30
-      }
+    '_id' = 2
+    'name': 'Mount Everest',
+    'date': 'Date(2018, 5, 15)',
+    'description': 'Snow',
+    'map_year': 2018,
+    'x_coord': 15,
+    'y_coord': 30
+}
 
 poi3 = {
-		  '_id' = 3
-          'name': 'Tampa Bay',
-          'date': 'Date(2018, 8, 3)',
-          'description': 'Hot',
-          'map_year': 2018,
-          'x_coord': 50,
-          'y_coord': 82
-      }
+    '_id' = 3
+    'name': 'Tampa Bay',
+    'date': 'Date(2018, 8, 3)',
+    'description': 'Hot',
+    'map_year': 2018,
+    'x_coord': 50,
+    'y_coord': 82
+}
 
 stories = [
-			{
-			  '_id': 21,
-			  'story_name': 'Angad Goes to Wisconsin',
-			},
-			{
-			  '_id': 22,
-			  'story_name': 'Amanda Takes her Medicine',
-			},
-			{
-			  '_id': 23,
-			  'story_name': 'Jeffy Goes to Fashion Show',
-			},
-			{
-			  '_id': 24,
-			  'story_name': 'Time for Hack4Impact',
-			}
-		  ]
-			
-		  
-story = {
-			'_id': 25
-			'story_name' : 'Jeffy Discovers the Dark side of the Moon'
-		}
+    {
+      '_id': 21,
+      'story_name': 'Angad Goes to Wisconsin',
+    },
+    {
+      '_id': 22,
+      'story_name': 'Amanda Takes her Medicine',
+    },
+    {
+      '_id': 23,
+      'story_name': 'Jeffy Goes to Fashion Show',
+    },
+    {
+      '_id': 24,
+      'story_name': 'Time for Hack4Impact',
+    }
+]
+            
+          
+    story = {
+    '_id': 25
+    'story_name' : 'Jeffy Discovers the Dark side of the Moon'
+}
 
