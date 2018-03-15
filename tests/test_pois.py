c6a0318c15a44eedcd22a2526a1d61e1ab36ef1d
Prashant S Pokhriyal <psp@wirelessprv-10-192-237-25.near.illinois.edu>
2018-03-15 01:17:06 -0500

    Updates include removing table querying

--- tests/test_pois.py
+++ tests/test_pois.py
@@ -9,9 +9,9 @@ from flask import jsonify
 import json
 from datetime import date
 
-#GOOD
+#Complete params
 
-poi_edit1_good = {
+poi_create_complete = {
     'name': 'Morrow Plots',
     'date': '2018-03-05',
     'description': 'Just corn here',
@@ -33,7 +33,7 @@ poi_edit1_good = {
     'story_ids': [1, 2]
 }
 
-poi_edit2_good = {
+poi_update_complete = {
     'name': 'Morrow Plots',
     'date': '2018-03-05',
     'description': 'Just corn here',
@@ -52,13 +52,11 @@ poi_edit2_good = {
     'story_ids': [1, 2]
 }
 
-#OK (expected to all pass)
+#Permissible (only required attributes, optional ones not included)
 
-poi_edit1_ok = {
+poi_create_permissible = {
     #This one needs EITHER map_year OR story_ids
     'name': 'Morrow Plots',
-    #Technically date is not required, but frontend will automatically send in date as 1/1/mapyear
-    #Otherwise there would be an error when running this test
     'date': '1856-01-01',
     'description': 'Just corn here',
     'map_year': 1856,
@@ -69,7 +67,7 @@ poi_edit1_ok = {
     'story_ids': []
 }
 
-poi_edit2_ok = {
+poi_edit_permissible = {
     'name': 'Morrow Plots',
     #Technically date is not required, but frontend will automatically send in date as 1/1/mapyear
     #Otherwise there would be an error when running this test
@@ -80,13 +78,10 @@ poi_edit2_ok = {
     'story_ids': []
 }
 
-#BAD
+#Invalid
 
-poi_edit1_bad = {
-    #Does not have name
-    #Does not have description
-    #Does not have map_year
-    #Does not have x-coord or y-coord
+poi_create_invalid = {
+    #Does not have name, description, map_year, x_coord, y_coord
     'name': '',
     'date': '01-01-00',
     'description': '',
@@ -98,35 +93,32 @@ poi_edit1_bad = {
     'story_ids': []
 }
 
-map_year = 2018
-story_id = 2
-
-map_year_bad = 10000
-story_id_bad = 10000
-poi_id_bad = 10000
+map_year_invalid = 0
+story_id_invalid = 0
+poi_id_invalid = 0
 
 os.environ['POI_ID'] = '1' #initialize POI_ID as environment variable
 
 class POITests(unittest.TestCase):
 
     def test0_1_create_poi(self):
-        r = requests.post('http://127.0.0.1:5000/pois', json=poi_edit1_good)
+        r = requests.post('http://127.0.0.1:5000/pois', json=poi_create_complete)
         response = r.json()
         self.assertEqual(response['code'], 201)
-        self.assertEqual(response['result']['poi']['name'], poi_edit1_good['name'])
+        self.assertEqual(response['result']['poi']['name'], poi_create_complete['name'])
         # self.assertEqual(response['result']['poi']['date'], poi_edit['date'])
-        self.assertEqual(response['result']['poi']['description'], poi_edit1_good['description'])
-        self.assertEqual(response['result']['poi']['map_year'], poi_edit1_good['map_year'])
-        self.assertEqual(response['result']['poi']['x_coord'], poi_edit1_good['x_coord'])
-        self.assertEqual(response['result']['poi']['y_coord'], poi_edit1_good['y_coord'])
+        self.assertEqual(response['result']['poi']['description'], poi_create_complete['description'])
+        self.assertEqual(response['result']['poi']['map_year'], poi_create_complete['map_year'])
+        self.assertEqual(response['result']['poi']['x_coord'], poi_create_complete['x_coord'])
+        self.assertEqual(response['result']['poi']['y_coord'], poi_create_complete['y_coord'])
         for i in range(len(response['result']['poi']['links'])):
-            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_edit1_good['links'][i]['link_url'])
-            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_edit1_good['links'][i]['display_name'])
+            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_create_complete['links'][i]['link_url'])
+            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_create_complete['links'][i]['display_name'])
         for j in range(len(response['result']['poi']['media'])):
-            self.assertEqual(response['result']['poi']['media'][j]['content_url'], poi_edit1_good['media'][j]['content_url'])
-            self.assertEqual(response['result']['poi']['media'][j]['caption'], poi_edit1_good['media'][j]['caption'])
+            self.assertEqual(response['result']['poi']['media'][j]['content_url'], poi_create_complete['media'][j]['content_url'])
+            self.assertEqual(response['result']['poi']['media'][j]['caption'], poi_create_complete['media'][j]['caption'])
         response_story_ids = [k['_id'] for k in response['result']['poi']['stories']]
-        self.assertEqual(sorted(response_story_ids), sorted(poi_edit1_good['story_ids']))
+        self.assertEqual(sorted(response_story_ids), sorted(poi_create_complete['story_ids']))
         os.environ['POI_ID'] = str(response['result']['poi']['_id'])
 
     def test0_2_get_poi_by_id(self, poi_id=0):
@@ -136,134 +128,133 @@ class POITests(unittest.TestCase):
         response = r.json()
         self.assertEqual(response['code'], 200)
         self.assertEqual(response['result']['poi']['_id'], poi_id)
-        query_response = POI.query.get(poi_id)
-        self.assertEqual(response['result']['poi']['name'], query_response.name)
-        # self.assertEqual(response['result']['poi']['date'], query_response.date)
-        self.assertEqual(response['result']['poi']['description'], query_response.description)
-        self.assertEqual(response['result']['poi']['map_year'], query_response.map_year)
-        self.assertEqual(response['result']['poi']['x_coord'], query_response.x_coord)
-        self.assertEqual(response['result']['poi']['y_coord'], query_response.y_coord)
+
+        self.assertEqual(response['result']['poi']['name'], poi_create_complete['name'])
+        # self.assertEqual(response['result']['poi']['date']
+        self.assertEqual(response['result']['poi']['description'], poi_create_complete['description'])
+        self.assertEqual(response['result']['poi']['map_year'], poi_create_complete['map_year'])
+        self.assertEqual(response['result']['poi']['x_coord'], poi_create_complete['x_coord'])
+        self.assertEqual(response['result']['poi']['y_coord'], poi_create_complete['y_coord'])
 
         #Account for time zones (see above commented out line)
         #Future Testing needed for links, media, stories (would require several loops based on json response)
 
-    def test0_3_get_pois(self):
+    def test0_3_get_pois_by_map_year(self):
+        map_year = poi_create_complete['map_year']
+
+        #get request by map_year
+
         r1 = requests.get('http://127.0.0.1:5000/pois?map_year={}'.format(map_year))
-        r2 = requests.get('http://127.0.0.1:5000/pois?story_id={}'.format(story_id))
         response1 = r1.json()
-        response2 = r2.json()
-        #map_year
+
         self.assertEqual(response1['code'], 200)
+
         for i in response1['result']['pois']:
             self.test0_2_get_poi_by_id(poi_id = i['_id'])
             self.assertEqual(i['map_year'], map_year)
-        # story_id
-        self.assertEqual(response2['code'], 200)
-        for i in response2['result']['pois']:
-            stories_by_poi_id = StoryPOI.query.filter(StoryPOI.poi_id == i['_id'])
-            story_ids1 = [j.to_dict()['story_id'] for j in stories_by_poi_id]
-            story_ids2 = [k['_id'] for k in i['stories']]
-            self.assertEqual(sorted(story_ids1), sorted(story_ids2))
+
+    def test0_3_get_pois_by_story_id(self):
+        story_ids = poi_create_complete['story_ids']
+
+        #get request by story_id
+
+        for curr_story_id in story_ids:
+            r2 = requests.get('http://127.0.0.1:5000/pois?story_id={}'.format(curr_story_id))
+            response2 = r2.json()
+            self.assertEqual(response2['code'], 200)
+            #created_object_included is used to determine if the POI we previously created is included
+            #in the list of POIs returned after we get request based on story_ids
+            created_object_included = False
+            for curr_poi in response2['result']['pois']:
+                if curr_poi['name'] == poi_create_complete['name']:
+                    created_object_included = True
+            self.assertTrue(created_object_included)
 
     def test0_4_update_poi(self):
         poi_id = int(os.environ.get('POI_ID'))
-        r = requests.put('http://127.0.0.1:5000/pois/{}'.format(poi_id), json=poi_edit2_good)
+        r = requests.put('http://127.0.0.1:5000/pois/{}'.format(poi_id), json=poi_update_complete)
         response = r.json()
         self.assertEqual(response['code'], 200)
-        self.assertEqual(response['result']['poi']['name'], poi_edit2_good['name'])
-        # self.assertEqual(response['result']['poi']['date'], poi_edit2_good['date'])
-        self.assertEqual(response['result']['poi']['description'], poi_edit2_good['description'])
+        self.assertEqual(response['result']['poi']['name'], poi_update_complete['name'])
+        # self.assertEqual(response['result']['poi']['date'], poi_update_complete['date'])
+        self.assertEqual(response['result']['poi']['description'], poi_update_complete['description'])
         for i in range(len(response['result']['poi']['links'])):
-            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_edit2_good['links'][i]['link_url'])
-            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_edit2_good['links'][i]['display_name'])
+            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_update_complete['links'][i]['link_url'])
+            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_update_complete['links'][i]['display_name'])
         for i in range(len(response['result']['poi']['media'])):
-            self.assertEqual(response['result']['poi']['media'][i]['content_url'], poi_edit2_good['media'][i]['content_url'])
-            self.assertEqual(response['result']['poi']['media'][i]['caption'], poi_edit2_good['media'][i]['caption'])
+            self.assertEqual(response['result']['poi']['media'][i]['content_url'], poi_update_complete['media'][i]['content_url'])
+            self.assertEqual(response['result']['poi']['media'][i]['caption'], poi_update_complete['media'][i]['caption'])
         response_story_ids = [i['_id'] for i in response['result']['poi']['stories']]
-        self.assertEqual(sorted(response_story_ids), sorted(poi_edit2_good['story_ids']))
+        self.assertEqual(sorted(response_story_ids), sorted(poi_update_complete['story_ids']))
 
     def test0_5_delete_poi(self):
         poi_id = int(os.environ.get('POI_ID'))
         r = requests.delete('http://127.0.0.1:5000/pois/{}'.format(poi_id))
         response = r.json()
         self.assertEqual(response['code'], 200)
-        POI_query = POI.query.get(poi_id)
-        Media_query = Media.query.filter(Media.poi_id == poi_id)
-        Link_query = Link.query.filter(Link.poi_id == poi_id)
-        StoryPOI_query = StoryPOI.query.filter(StoryPOI.poi_id == poi_id)
-        self.assertEqual(POI_query, None)
-        self.assertEqual(Media_query.count(), 0)
-        self.assertEqual(Link_query.count(), 0)
-        self.assertEqual(StoryPOI_query.count(), 0)
 
-    def test1_1_ok(self):
-        r = requests.post('http://127.0.0.1:5000/pois', json=poi_edit1_ok)
+        r2 = requests.get('http://127.0.0.1:5000/pois/{}'.format(poi_id))
+        response2 = r2.json()
+        self.assertEqual(response2['code'], 404)
+
+    def test1_1_permissible(self):
+        r = requests.post('http://127.0.0.1:5000/pois', json=poi_create_permissible)
         response = r.json()
         self.assertEqual(response['code'], 201)
-        self.assertEqual(response['result']['poi']['name'], poi_edit1_ok['name'])
+        self.assertEqual(response['result']['poi']['name'], poi_create_permissible['name'])
         # self.assertEqual(response['result']['poi']['date'], poi_edit['date'])
-        self.assertEqual(response['result']['poi']['description'], poi_edit1_ok['description'])
-        self.assertEqual(response['result']['poi']['map_year'], poi_edit1_ok['map_year'])
-        self.assertEqual(response['result']['poi']['x_coord'], poi_edit1_ok['x_coord'])
-        self.assertEqual(response['result']['poi']['y_coord'], poi_edit1_ok['y_coord'])
+        self.assertEqual(response['result']['poi']['description'], poi_create_permissible['description'])
+        self.assertEqual(response['result']['poi']['map_year'], poi_create_permissible['map_year'])
+        self.assertEqual(response['result']['poi']['x_coord'], poi_create_permissible['x_coord'])
+        self.assertEqual(response['result']['poi']['y_coord'], poi_create_permissible['y_coord'])
         for i in range(len(response['result']['poi']['links'])):
-            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_edit1_ok['links'][i]['link_url'])
-            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_edit1_ok['links'][i]['display_name'])
+            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_create_permissible['links'][i]['link_url'])
+            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_create_permissible['links'][i]['display_name'])
         for j in range(len(response['result']['poi']['media'])):
-            self.assertEqual(response['result']['poi']['media'][j]['content_url'], poi_edit1_ok['media'][j]['content_url'])
-            self.assertEqual(response['result']['poi']['media'][j]['caption'], poi_edit1_ok['media'][j]['caption'])
+            self.assertEqual(response['result']['poi']['media'][j]['content_url'], poi_create_permissible['media'][j]['content_url'])
+            self.assertEqual(response['result']['poi']['media'][j]['caption'], poi_create_permissible['media'][j]['caption'])
         response_story_ids = [k['_id'] for k in response['result']['poi']['stories']]
-        self.assertEqual(sorted(response_story_ids), sorted(poi_edit1_ok['story_ids']))
+        self.assertEqual(sorted(response_story_ids), sorted(poi_create_permissible['story_ids']))
         os.environ['POI_ID'] = str(response['result']['poi']['_id'])
 
-    def test1_4_ok(self):
+    def test1_4_permissible(self):
         poi_id = int(os.environ.get('POI_ID'))
-        r = requests.put('http://127.0.0.1:5000/pois/{}'.format(poi_id), json=poi_edit2_ok)
+        r = requests.put('http://127.0.0.1:5000/pois/{}'.format(poi_id), json=poi_edit_permissible)
         response = r.json()
         self.assertEqual(response['code'], 200)
-        self.assertEqual(response['result']['poi']['name'], poi_edit2_ok['name'])
-        # self.assertEqual(response['result']['poi']['date'], poi_edit2_good['date'])
-        self.assertEqual(response['result']['poi']['description'], poi_edit2_ok['description'])
+        self.assertEqual(response['result']['poi']['name'], poi_edit_permissible['name'])
+        # self.assertEqual(response['result']['poi']['date'], poi_update_complete['date'])
+        self.assertEqual(response['result']['poi']['description'], poi_edit_permissible['description'])
         for i in range(len(response['result']['poi']['links'])):
-            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_edit2_ok['links'][i]['link_url'])
-            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_edit2_ok['links'][i]['display_name'])
+            self.assertEqual(response['result']['poi']['links'][i]['link_url'], poi_edit_permissible['links'][i]['link_url'])
+            self.assertEqual(response['result']['poi']['links'][i]['display_name'], poi_edit_permissible['links'][i]['display_name'])
         for i in range(len(response['result']['poi']['media'])):
-            self.assertEqual(response['result']['poi']['media'][i]['content_url'], poi_edit2_ok['media'][i]['content_url'])
-            self.assertEqual(response['result']['poi']['media'][i]['caption'], poi_edit2_ok['media'][i]['caption'])
+            self.assertEqual(response['result']['poi']['media'][i]['content_url'], poi_edit_permissible['media'][i]['content_url'])
+            self.assertEqual(response['result']['poi']['media'][i]['caption'], poi_edit_permissible['media'][i]['caption'])
         response_story_ids = [i['_id'] for i in response['result']['poi']['stories']]
-        self.assertEqual(sorted(response_story_ids), sorted(poi_edit2_ok['story_ids']))
-    #
-    # #Below...2
-    # #Tests that should fail or break
-    # #Tests that should fail or break
-    # #Tests that should fail or break
-    # #Tests that should fail or break
-    # #Tests that should fail or break
-    # #Tests that should fail or break
-    #
+        self.assertEqual(sorted(response_story_ids), sorted(poi_edit_permissible['story_ids']))
 
-    def test2_1_bad(self):
-        r = requests.post('http://127.0.0.1:5000/pois', json= poi_edit1_bad)
+    def test2_1_invalid(self):
+        r = requests.post('http://127.0.0.1:5000/pois', json= poi_create_invalid)
         response = r.json()
         self.assertEqual(response['code'], 422)
 
-    def test2_2_bad(self, poi_id=0):
-        r = requests.get('http://127.0.0.1:5000/pois/{}'.format(poi_id_bad))
+    def test2_2_invalid(self, poi_id=0):
+        r = requests.get('http://127.0.0.1:5000/pois/{}'.format(poi_id_invalid))
         response = r.json()
         self.assertEqual(response['code'], 404)
 
-    def test2_3_bad(self):
-        r1 = requests.get('http://127.0.0.1:5000/pois?map_year={}'.format(map_year_bad))
-        r2 = requests.get('http://127.0.0.1:5000/pois?story_id={}'.format(story_id_bad))
+    def test2_3_invalid(self):
+        r1 = requests.get('http://127.0.0.1:5000/pois?map_year={}'.format(map_year_invalid))
+        r2 = requests.get('http://127.0.0.1:5000/pois?story_id={}'.format(story_id_invalid))
         response1 = r1.json()
         response2 = r2.json()
-        #map_year_bad
+        #map_year_invalid
         self.assertEqual(response1['code'], 404)
         self.assertEqual(response2['code'], 404)
 
-    def test2_5_bad(self):
-        # poi_id = int(os.environ.get('POI_ID'))
-        r = requests.delete('http://127.0.0.1:5000/pois/{}'.format(poi_id_bad))
+    def test2_5_invalid(self):
+        r = requests.delete('http://127.0.0.1:5000/pois/{}'.format(poi_id_invalid))
         response = r.json()
         self.assertEqual(response['code'], 404)
 
