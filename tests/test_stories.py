4af29931f19fe932b1056d893491d20234f523c0
wangworks <policetoed@gmail.com>
2018-03-15 00:24:49 -0500

    took out second poi

--- tests/test_stories.py
+++ tests/test_stories.py
@@ -17,21 +17,16 @@ poi1 = {
     'y_coord': 43
 }
 
-poi2 = {
-    'name': 'Mount Everest',
-    'date': '2018-05-15',
-    'description': 'Snow',
-    'map_year': 2018,
-    'x_coord': 15,
-    'y_coord': 30
-}
-
 story_with_no_pois = {
     'story_name' : 'Jeffy Discovers the Dark side of the Moon'
 }
 
 story_with_pois = {
     'story_name' : 'Angad Goes to Wisconsin',
+    'poi_ids' : []
+}
+
+story_edit_pois = {
     'poi_ids' : []
 }
 
@@ -43,15 +38,10 @@ story_empty = {
 
 }
 
-story_edit_pois = {
-    'poi_ids' : []
-}
 
-
-os.environ['STORY_ID_WITH_NO_POIS'] = '1' # this one doesn't have pois
-os.environ['STORY_ID_WITH_POIS'] = '2' # this one has the pois
+os.environ['STORY_ID_WITH_NO_POIS'] = '1'
+os.environ['STORY_ID_WITH_POIS'] = '2'
 os.environ['POI_ID_1'] = '1'
-os.environ['POI_ID_2'] = '2'
 
 class POITests(unittest.TestCase):
 
@@ -69,12 +59,6 @@ class POITests(unittest.TestCase):
         os.environ['POI_ID_1'] = str(response['result']['poi']['_id'])
         poi_id_1 = int(response['result']['poi']['_id'])
 
-        r = requests.post('http://127.0.0.1:5000/pois', json=poi2)
-        response = r.json()
-        story_with_pois['poi_ids'].append(response['result']['poi']['_id'])
-        os.environ['POI_ID_2'] = str(response['result']['poi']['_id'])
-        poi_id_2 = int(response['result']['poi']['_id'])
-
         r = requests.post('http://127.0.0.1:5000/stories', json=story_with_pois)
         response = r.json()
         self.assertEqual(response['code'], 201)
@@ -83,11 +67,6 @@ class POITests(unittest.TestCase):
         story_id_with_pois = int(response['result']['story']['_id'])
 
         r = requests.get('http://127.0.0.1:5000/pois/{}'.format(poi_id_1))
-        response = r.json()
-        story = next((s for s in response['result']['poi']['stories'] if s['_id'] == story_id_with_pois), None)
-        self.assertEqual(story['story_name'], story_with_pois['story_name'])
-
-        r = requests.get('http://127.0.0.1:5000/pois/{}'.format(poi_id_2))
         response = r.json()
         story = next((s for s in response['result']['poi']['stories'] if s['_id'] == story_id_with_pois), None)
         self.assertEqual(story['story_name'], story_with_pois['story_name'])
@@ -108,17 +87,8 @@ class POITests(unittest.TestCase):
         story_id_with_no_pois = int(os.environ.get('STORY_ID_WITH_NO_POIS'))
         story_id_with_pois = int(os.environ.get('STORY_ID_WITH_POIS'))
         poi_id_1 = int(os.environ.get('POI_ID_1'))
-        poi_id_2 = int(os.environ.get('POI_ID_2'))
 
         r = requests.get('http://127.0.0.1:5000/stories?poi_id={}'.format(poi_id_1))
-        response = r.json()
-        self.assertEqual(response['code'], 200)
-        story = next((s for s in response['result']['stories'] if s['_id'] == story_id_with_pois), None)
-        self.assertEqual(story['story_name'], story_with_pois['story_name'])
-        story = next((s for s in response['result']['stories'] if s['_id'] == story_id_with_no_pois), None)
-        self.assertEqual(story, None)
-
-        r = requests.get('http://127.0.0.1:5000/stories?poi_id={}'.format(poi_id_2))
         response = r.json()
         self.assertEqual(response['code'], 200)
         story = next((s for s in response['result']['stories'] if s['_id'] == story_id_with_pois), None)
@@ -169,7 +139,6 @@ class POITests(unittest.TestCase):
         story_id_with_no_pois = int(os.environ.get('STORY_ID_WITH_NO_POIS'))
         story_id_with_pois = int(os.environ.get('STORY_ID_WITH_POIS'))
         poi_id_1 = int(os.environ.get('POI_ID_1'))
-        poi_id_2 = int(os.environ.get('POI_ID_2'))
 
         r = requests.delete('http://127.0.0.1:5000/stories/{}'.format(story_id_with_no_pois))
         response_put = r.json()
@@ -187,7 +156,6 @@ class POITests(unittest.TestCase):
         self.assertEqual(story, None)
 
         r = requests.delete('http://127.0.0.1:5000/pois/{}'.format(poi_id_1))
-        r = requests.delete('http://127.0.0.1:5000/pois/{}'.format(poi_id_2))
 
     def test9_delete_nonexistent_story(self):
         r = requests.delete('http://127.0.0.1:5000/stories/{}'.format(int(0)))
