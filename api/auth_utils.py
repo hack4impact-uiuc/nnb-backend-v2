a2c2c2fb8babf9a9c38d22cb04c3489da3471883
Alex <awu1337@gmail.com>
2018-04-03 22:53:56 -0500

    fix spacing in auth_utils

--- api/auth_utils.py
+++ api/auth_utils.py
@@ -13,8 +13,13 @@ MIN_REQUIRED_PASSWORD_LEN = 7
 def hash_password(password, salt):
     password_bytes = bytearray(password, encoding='utf-8')
     salt_bytes = bytes.fromhex(salt)
-    dk = hashlib.pbkdf2_hmac(HASH_ALGORITHM, password_bytes, salt_bytes,
-                                NUM_HASH_ITERATIONS, HASH_OUTPUT_LEN)
+    dk = hashlib.pbkdf2_hmac(
+        HASH_ALGORITHM,
+        password_bytes,
+        salt_bytes,
+        NUM_HASH_ITERATIONS,
+        HASH_OUTPUT_LEN
+    )
     return dk.hex()
 
 def generate_salt():
@@ -24,7 +29,9 @@ def validate_password_requirements(password):
     failure_message = []
 
     if len(password) < MIN_REQUIRED_PASSWORD_LEN:
-        failure_message.append('password must be at least {} characters long'\
-                .format(MIN_REQUIRED_PASSWORD_LEN))
+        failure_message.append(
+            'password must be at least {} characters long'\
+            .format(MIN_REQUIRED_PASSWORD_LEN)
+        )
     
     return '\n'.join(failure_message)
