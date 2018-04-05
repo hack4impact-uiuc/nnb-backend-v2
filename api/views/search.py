70bdef78072d50c6ae4a96c178e9e94d0b200966
wangworks <policetoed@gmail.com>
2018-04-05 00:04:16 -0500

    cooool both name and description searches work

--- api/views/search.py
+++ api/views/search.py
@@ -11,10 +11,20 @@ mod = Blueprint('search', __name__)
 def search_pois():
     data = request.args
     pois = []
+    pois1 = []
+    pois2 = []
 
-    if data.get('name') is not None:
-        pois = POI.query.filter(POI.name.like(data['name']))
+    if data.get('q') is not None:
+        if data.get('name') is not None:
+            if data.get('name') == "True":
+                pois1 = POI.query.filter(POI.name.contains(data['q']))
+        if data.get('description') is not None:
+            if data.get('description') == "True":
+                pois2 = POI.query.filter(POI.description.contains(data['q']))
 
-    if pois.count() == 0:
+    if pois1.count() == 0 and pois2.count() == 0:
         return create_response(status=404, message='No POIs found')
-    return create_response({'pois_list': [p.to_dict() for p in pois]})
+    list1 = [p.to_dict() for p in pois1]
+    list2 = [p.to_dict() for p in pois2]
+    pois = list1 + list2
+    return create_response({'pois_list': pois})
