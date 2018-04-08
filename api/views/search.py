a6e793f76f3ec71232b24babef9ddf54876e2ed0
wangworks <policetoed@gmail.com>
2018-04-08 14:29:20 -0500

    fixed comments from pr; everything works

--- api/views/search.py
+++ api/views/search.py
@@ -1,30 +1,33 @@
 from api import app, db
-from api.models import Story, StoryPOI, POI
-from api.utils import create_response, row_constructor
+from api.models import POI
+from api.utils import create_response
 from flask import Blueprint, request
 
-SEARCH_URL = "/search"
+SEARCH_POIS_URL = "/search/pois"
 
 mod = Blueprint('search', __name__)
 
-@app.route(SEARCH_URL, methods=['GET'])
+@app.route(SEARCH_POIS_URL, methods=['GET'])
 def search_pois():
     data = request.args
-    pois = []
-    pois1 = []
-    pois2 = []
+    pois_by_name = []
+    pois_by_description = []
 
     if data.get('q') is not None:
         if data.get('name') is not None:
-            if data.get('name') == "True":
-                pois1 = POI.query.filter(POI.name.contains(data['q']))
+            if data.get('name') == "true":
+                pois_by_name = POI.query.filter(POI.name.contains(data['q']))
+        else:
+            return create_response(status=404, message='Missing name parameter (true/false)')
         if data.get('description') is not None:
-            if data.get('description') == "True":
-                pois2 = POI.query.filter(POI.description.contains(data['q']))
+            if data.get('description') == "true":
+                pois_by_description = POI.query.filter(POI.description.contains(data['q']))
+        else:
+            return create_response(status=404, message='Missing description parameter (true/false)')
+    else:
+        return create_response(status=404, message='Missing query parameter')
 
-    if pois1.count() == 0 and pois2.count() == 0:
-        return create_response(status=404, message='No POIs found')
-    list1 = [p.to_dict() for p in pois1]
-    list2 = [p.to_dict() for p in pois2]
-    pois = list1 + list2
-    return create_response({'pois_list': pois})
+    pois_by_name_list = [p.to_dict() for p in pois_by_name]
+    pois_by_description_list = [p.to_dict() for p in pois_by_description]
+    pois = pois_by_name_list + pois_by_description_list
+    return create_response({'pois': pois})
