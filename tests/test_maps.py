b01cde9ee442151da526f9e0d279f8b64ecc5580
Andrew <andy@DESKTOP-2163CNP.localdomain>
2018-04-27 19:16:40 -0500

    error free, need to separate test functions still

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -10,80 +10,73 @@ from flask import jsonify
 import json
 
 # json objects to be tested
-map_add_json_1 = {
+map_good_json = {
     "map_year" : 1669,
     "image_url": 'http://www.arizona-leisure.com/gfx/maps/valley-sun-map-760.gif'
- }
-
-map_add_json_2 = {
-    "map_year" : 1670,
-    "image_url": 'http://www.fultonranchtownecenter.com/Post/sections/11/Images/FR_MasterPlan.jpg',
 }
 
-map_add_json_3 = {
-    "map_year" : 3000,
-    "image_url" : "https://pcavote.files.wordpress.com/2015/12/jonas-brothers-reunion.jpg"
-}
+map_bad_jsons = []
+map_bad_jsons.append(
+    {
+        "image_url": 'http://www.fultonranchtownecenter.com/Post/sections/11/Images/FR_MasterPlan.jpg'
+    }
+)
+map_bad_jsons.append(
+    {
+        "map_year": 2000
+    }
+)
+map_bad_jsons.append({})
 
-map_add_json_bad_1 = {
-    "image_url": 'http://www.fultonranchtownecenter.com/Post/sections/11/Images/FR_MasterPlan.jpg'
-}
+poi_jsons = []
+poi_jsons.append(
+    {
+        "name" : "The battle of the Bulge",
+        "date" : "5/20",
+        "description" : "people died",
+        "map_year" : 3000,
+        "x_coord" : "5",
+        "y_coord" : "7",
+        'links': [],
+        'media': [],
+        'story_ids': []
+    }
+)
+poi_jsons.append(
+    {
+        "name" : "The Great Herbal Fire",
+        "date" : "4/20",
+        "description" : "An uplifting experience for many people",
+        "map_year" : 3000,
+        "x_coord" : "7",
+        "y_coord" : "5",
+        'links': [],
+        'media': [],
+        'story_ids': []
+    }
+)
+poi_jsons.append(
+    {
+        "name" : "Nithin Becomes a Man",
+        "date" : "2/4",
+        "description" : "Nothing really",
+        "map_year" : 3000,
+        "x_coord" : "8",
+        "y_coord" : "9",
+        'links': [],
+        'media': [],
+        'story_ids': []
+    }
+)
 
-map_add_json_bad_2 = {
-    "map_year": 2000
-}
-
-map_add_json_bad_3 = {
-
-}
-
-poi_add_json_1 = {
-    "name" : "The battle of the Bulge",
-    "date" : "5/20",
-    "description" : "people died",
-    "map_year" : 3000,
-    "x_coord" : "5",
-    "y_coord" : "7",
-    'links': [],
-    'media': [],
-    'story_ids': []
-}
-
-poi_add_json_2 = {
-    "name" : "The Great Herbal Fire",
-    "date" : "4/20",
-    "description" : "An uplifting experience for many people",
-    "map_year" : 3000,
-    "x_coord" : "7",
-    "y_coord" : "5",
-    'links': [],
-    'media': [],
-    'story_ids': []
-}
-
-poi_add_json_3 = {
-    "name" : "Nithin Becomes a Man",
-    "date" : "2/4",
-    "description" : "Nothing really",
-    "map_year" : 3000,
-    "x_coord" : "8",
-    "y_coord" : "9",
-    'links': [],
-    'media': [],
-    'story_ids': []
-}
-
-def find_map_occurence(map_obj):
+# assigns map_obj a unique year not in the map list
+def find_unique_year(map_obj):
     data = requests.get('http://127.0.0.1:5000/maps')
     map_year = map_obj['map_year']
-    map_year_list = list(filter(lambda x: x['map_year'], data.json()['result']['maps']))
+    map_year_list = [x['map_year'] for x in data.json()['result']['maps']]
     while map_year in map_year_list:
-        map_year = random.randInt(1800,2018)
+        map_year +=1
     map_obj['map_year'] = map_year
-
-# def find_non_existent_id():
-#     _id = 13
-
 
 
 class MapTests(unittest.TestCase):
@@ -91,18 +84,20 @@ class MapTests(unittest.TestCase):
 
     def test_map_post_get(self):
         # testing 1 post and 1 get
-        find_map_occurence(map_add_json_1)
-        post_data = requests.post('http://127.0.0.1:5000/maps', json= map_add_json_1)
+        find_unique_year(map_good_json)
+        post_data = requests.post('http://127.0.0.1:5000/maps', json= map_good_json)
         get_data = requests.get('http://127.0.0.1:5000/maps')
 
-        post_data_body = post_data.json()['result']['map']
-        get_data_body = get_data.json()['result']['maps']
-        self.assertEqual(post_data.json()['code'], 201)
-        self.assertEqual(get_data.json()['code'], 200)
+        post_data_json = post_data.json()
+        get_data_json = get_data.json()
+        post_data_body = post_data_json['result']['map']
+        get_data_body = get_data_json['result']['maps']
+        self.assertEqual(post_data_json['code'], 201)
+        self.assertEqual(get_data_json['code'], 200)
 
         map_id_list = [x['_id'] for x in get_data_body]
         map_posted = post_data_body['_id'] in map_id_list
-        self.assertEqual(True, map_posted)
+        self.assertTrue(map_posted)
 
         # is there a way to shorten this ?
 
@@ -120,17 +115,12 @@ class MapTests(unittest.TestCase):
         before_get = requests.get('http://127.0.0.1:5000/maps')
         before_size = len(before_get.json()['result']['maps'])
 
-        response = requests.post('http://127.0.0.1:5000/maps', json= map_add_json_bad_1)
-        self.assertEqual(response.json()['code'], 400)
-
-        response = requests.post('http://127.0.0.1:5000/maps', json= map_add_json_bad_2)
-        self.assertEqual(response.json()['code'], 400)
-
-        response = requests.post('http://127.0.0.1:5000/maps', json= map_add_json_bad_3)
-        self.assertEqual(response.json()['code'], 400)
+        for map_bad_json in map_bad_jsons:
+            response = requests.post('http://127.0.0.1:5000/maps', json= map_bad_json)
+            self.assertEqual(response.json()['code'], 400)
 
         id = 1
-        while id in list(filter(lambda x: x['_id'], before_get.json()['result']['maps'])):
+        while id in [x['_id'] for x in before_get.json()['result']['maps']]:
             id+=1
         response = requests.delete('http://127.0.0.1:5000/maps/{}'.format(id))
         self.assertEqual(response.json()['status'], 404)
@@ -143,28 +133,23 @@ class MapTests(unittest.TestCase):
         # is fully functional.
 
     def test_map_delete(self):
-
         # tests the delete functionality and checks that the pois are deleted as
         # well
-        find_map_occurence(map_add_json_3)
-        poi_add_json_1['map_year'] = map_add_json_3['map_year']
-        poi_add_json_2['map_year'] = map_add_json_3['map_year']
-        poi_add_json_3['map_year'] = map_add_json_3['map_year']
+        find_unique_year(map_good_json)
+        for poi_json in poi_jsons:
+            poi_json['map_year'] = map_good_json['map_year']
+            requests.post('http://127.0.0.1:5000/pois', json=poi_json)
 
-        requests.post('http://127.0.0.1:5000/pois', json = poi_add_json_1)
-        requests.post('http://127.0.0.1:5000/pois', json = poi_add_json_2)
-        requests.post('http://127.0.0.1:5000/pois', json = poi_add_json_3)
-
-        post_data = requests.post('http://127.0.0.1:5000/maps', json= map_add_json_3)
+        post_data = requests.post('http://127.0.0.1:5000/maps', json=map_good_json)
         post_data_body = post_data.json()['result']['map']
         id = post_data_body['_id']
         response = requests.delete('http://127.0.0.1:5000/maps/{}'.format(id))
         self.assertEqual(response.json()['status'], 200)
 
-        data = requests.get('http://127.0.0.1:5000/maps')
-        response_data = data.json()['result']['maps']
-        map_deleted = id not in list(filter(lambda x: x['_id'], get_data_body))
-        self.assertEqual(True,map_deleted)
+        get_data = requests.get('http://127.0.0.1:5000/maps')
+        get_data_body = get_data.json()['result']['maps']
+        map_deleted = id not in [x['_id'] for x in get_data_body]
+        self.assertTrue(map_deleted)
 
         pois_response = requests.get('http://127.0.0.1:5000/pois?map_year={}'.format(post_data_body['map_year']))
         self.assertEqual(pois_response.json()['code'], 404)
