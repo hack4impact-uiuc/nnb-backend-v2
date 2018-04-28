0463d3eccd02ae95c34d8af3cd2979f827134ab2
Andrew <andy@DESKTOP-2163CNP.localdomain>
2018-04-28 18:35:04 -0500

    finished but untested

--- tests/test_maps.py
+++ tests/test_maps.py
@@ -10,21 +10,24 @@ from flask import jsonify
 import json
 
 MAPS_URL = 'http://127.0.0.1:5000/maps'
+def id_to_url(map_id):
+    return MAPS_URL + '/{}'.format(map_id)
+
 # json objects to be tested
 good_map = {
-    "map_year" : 1669,
-    "image_url": 'http://www.arizona-leisure.com/gfx/maps/valley-sun-map-760.gif'
+    'map_year' : 1669,
+    'image_url': 'http://www.arizona-leisure.com/gfx/maps/valley-sun-map-760.gif'
 }
 
 bad_maps = []
 bad_maps.append(
     {
-        "image_url": 'http://www.fultonranchtownecenter.com/Post/sections/11/Images/FR_MasterPlan.jpg'
+        'image_url': 'http://www.fultonranchtownecenter.com/Post/sections/11/Images/FR_MasterPlan.jpg'
     }
 )
 bad_maps.append(
     {
-        "map_year": 2000
+        'map_year': 2000
     }
 )
 bad_maps.append({})
@@ -32,12 +35,12 @@ bad_maps.append({})
 pois = []
 pois.append(
     {
-        "name" : "The battle of the Bulge",
-        "date" : "5/20",
-        "description" : "people died",
-        "map_year" : 3000,
-        "x_coord" : "5",
-        "y_coord" : "7",
+        'name' : 'The battle of the Bulge',
+        'date' : '5/20',
+        'description' : 'people died',
+        'map_year' : 3000,
+        'x_coord' : '5',
+        'y_coord' : '7',
         'links': [],
         'media': [],
         'story_ids': []
@@ -45,12 +48,12 @@ pois.append(
 )
 pois.append(
     {
-        "name" : "The Great Herbal Fire",
-        "date" : "4/20",
-        "description" : "An uplifting experience for many people",
-        "map_year" : 3000,
-        "x_coord" : "7",
-        "y_coord" : "5",
+        'name' : 'The Great Herbal Fire',
+        'date' : '4/20',
+        'description' : 'An uplifting experience for many people',
+        'map_year' : 3000,
+        'x_coord' : '7',
+        'y_coord' : '5',
         'links': [],
         'media': [],
         'story_ids': []
@@ -58,12 +61,12 @@ pois.append(
 )
 pois.append(
     {
-        "name" : "Nithin Becomes a Man",
-        "date" : "2/4",
-        "description" : "Nothing really",
-        "map_year" : 3000,
-        "x_coord" : "8",
-        "y_coord" : "9",
+        'name' : 'Nithin Becomes a Man',
+        'date' : '2/4',
+        'description' : 'Nothing really',
+        'map_year' : 3000,
+        'x_coord' : '8',
+        'y_coord' : '9',
         'links': [],
         'media': [],
         'story_ids': []
@@ -80,8 +83,14 @@ def find_unique_year(map_obj):
     map_obj['map_year'] = map_year
 
 
+# this is a comprensive test suite for testing the map end points
 class MapTests(unittest.TestCase):
-    # this is a comprensive test suite for testing the map end points
+    def test_map(self):
+        self.map_id = -1
+        self.test_map_post()
+        self.test_map_get()
+        self.test_map_delete()
+    
     def test_map_post(self):
         # test good map
         find_unique_year(good_map)
@@ -91,6 +100,7 @@ class MapTests(unittest.TestCase):
         response_map = response_json['result']['map']
         self.assertEqual(response_map['map_year'], good_map['map_year'])
         self.assertEqual(response_map['image_url'], good_map['image_url'])
+        self.map_id = response_map['_id']
         # test bad maps
         for bad_map in bad_maps:
             result = requests.post(MAPS_URL, json=bad_map)
@@ -99,56 +109,31 @@ class MapTests(unittest.TestCase):
             result_data = result_json['result']
             self.assertEqual(bad_map, result_data)
 
-    # TODO
+    # checks if a map is in the get response maps list. helper for get
+    def id_in_maps(self, map_id):
+        response = requests.get(MAPS_URL)
+        response_json = response.json()
+        self.assertEqual(response_json['code'], 200)
+        response_maps = response_json['result']['maps']
+        for map in response_maps:
+            if map['_id'] == self.map_id:
+                self.assertEqual(map['map_year'], good_map['map_year'])
+                self.assertEqual(map['image_url'], good_map['image_url'])
+                return True
+        return False
+
+    # assumes post worked
     def test_map_get(self):
-        # testing 1 post and 1 get
-        get_data = requests.get(MAPS_URL)
-        get_data_json = get_data.json()
-        get_data_body = get_data_json['result']['maps']
-        self.assertEqual(get_data_json['code'], 200)
+        # testing good map is in list
+        self.assertTrue(self.id_in_maps(self.map_id))
 
-    # TODO
-    def test_map_delete_bad(self):
-        # test that the proper status_code is returned given bad requests
-        before_get = requests.get(MAPS_URL)
-        before_size = len(before_get.json()['result']['maps'])
-
-        for map_bad_json in bad_maps:
-            response = requests.post(MAPS_URL, json= map_bad_json)
-            self.assertEqual(response.json()['code'], 400)
-
-        id = 1
-        while id in [x['_id'] for x in before_get.json()['result']['maps']]:
-            id+=1
-        response = requests.delete('http://127.0.0.1:5000/maps/{}'.format(id))
-        self.assertEqual(response.json()['status'], 404)
-
-        after_get = requests.get(MAPS_URL)
-        after_size = len(after_get.json()['result']['maps'])
-        self.assertEqual(before_size,after_size)
-
-        # This test suite is under the assumption that the post_poi/get_poi
-        # is fully functional.
-
-    # TODO
-    def test_map_delete_good(self):
-        # tests the delete functionality and checks that the pois are deleted as
-        # well
-        find_unique_year(good_map)
-        for poi_json in pois:
-            poi_json['map_year'] = good_map['map_year']
-            requests.post('http://127.0.0.1:5000/pois', json=poi_json)
-
-        post_data = requests.post(MAPS_URL, json=good_map)
-        post_data_body = post_data.json()['result']['map']
-        id = post_data_body['_id']
-        response = requests.delete('http://127.0.0.1:5000/maps/{}'.format(id))
-        self.assertEqual(response.json()['status'], 200)
-
-        get_data = requests.get(MAPS_URL)
-        get_data_body = get_data.json()['result']['maps']
-        map_deleted = id not in [x['_id'] for x in get_data_body]
-        self.assertTrue(map_deleted)
-
-        pois_response = requests.get('http://127.0.0.1:5000/pois?map_year={}'.format(post_data_body['map_year']))
-        self.assertEqual(pois_response.json()['code'], 404)
+    # assumes post worked
+    def test_map_delete(self):
+        # test delete on nonexistent map
+        bad_response = requests.delete(id_to_url(-1))
+        self.assertEqual(bad_response.json()['status'], 404)
+        # test delete on good_map
+        response = requests.delete(id_to_url(self.map_id))
+        self.assertEqual(response.json()['code'], 200)
+        # check that good_map no longer in list
+        self.assertFalse(self.id_in_maps(self.map_id))
